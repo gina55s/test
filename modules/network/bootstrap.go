affdc9f5bf1941ca2bc8fc0395637261afe825c6
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-06-20 15:01:41 +0200

    Squashed 12 commits from 2019-06-20
    - Create notes_20192007.md
    - Update CODEOWNERS
    - Make binaries (#68)
    - setupwgtest.sh and User Network Object
    - Syntax errors
    - Update network.go
    - add network/ip package
    - remove unused code
    - fix some tests and add doc strings
    - add DeleteNetResource to networker interface
    - fix nibble tests
    - refactor veth address configuration

---
+++ modules/network/bootstrap.go
@@ -0,0 +1,100 @@
+package network
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/testv2/modules/network/bridge"
+
+	"github.com/vishvananda/netlink"
+)
+
+const (
+	defaultBridge = "test"
+)
+
+// Bootstrap creates the default bridge of 0-OS
+// it then walk over all pluggued network interfaces and attaches them to the bridge
+// one by one and try to get an IP. Bootstrap stops as soon as one of the interface receives and ip with a
+// default route
+func Bootstrap() error {
+
+	log.Info().Msg("Create default bridge")
+	br, err := bridge.New(defaultBridge)
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to create bridge %s", defaultBridge)
+		return err
+	}
+
+	links, err := interfaces()
+	if err != nil {
+		log.Error().Err(err).Msgf("failed to list interfaces")
+		return err
+	}
+
+	var defaultGW *netlink.Device
+
+	for _, device := range filterDevices(links) {
+		log.Info().Str("interface", device.Name).Msg("probe interface")
+		// TODO: support noautonic kernel params
+		if device.Name == "lo" {
+			continue
+		}
+
+		if !isVirtEth(device.Name) {
+			if !isPlugged(device.Name) {
+				log.Info().Str("interface", device.Name).Msg("interface is not plugged in, skipping")
+				continue
+			}
+		}
+
+		// TODO: see if we need to set the if down
+		if err := netlink.LinkSetUp(device); err != nil {
+			log.Info().Str("interface", device.Name).Msg("failed to bring interface up")
+			continue
+		}
+
+		log.Info().Str("interface", device.Name).Msg("attach interface to default bridge")
+		if err := bridge.AttachNic(device, br); err != nil {
+			log.Warn().Err(err).
+				Str("device", device.Name).
+				Str("bridge", br.Name).
+				Msg("fail to attach device to bridge")
+			continue
+		}
+
+		log.Info().Str("interface", device.Name).Msg("start dhcp probing")
+		valid, err := dhcpProbe(br.Name)
+		if err != nil {
+			log.Warn().Err(err).Str("device", device.Name).Msg("dhcp probing unexpected error")
+			if err := bridge.DetachNic(br); err != nil {
+				log.Warn().Err(err).Str("device", device.Name).Msg("error detaching device from default bridge")
+			}
+			continue
+		}
+
+		if valid {
+			defaultGW = device
+			break
+		} else {
+			if err := bridge.DetachNic(br); err != nil {
+				log.Warn().Err(err).Str("device", device.Name).Msg("error detaching device from default bridge")
+			}
+		}
+	}
+
+	if defaultGW == nil {
+		err = fmt.Errorf("no interface with default gateway found")
+		log.Error().Err(err).Msg("cannot configure network")
+		return err
+	}
+
+	log.Info().Str("device", defaultGW.Name).Msg("default gateway found")
+	return nil
+}
+
+// DefaultBridgeName return the name of the default bridge
+// created by the network bootstrap
+func DefaultBridgeName() string {
+	return defaultBridge
+}
--- modules/network/networker.go
+++ modules/network/networker.go
@@ -7,20 +7,27 @@ import (
 
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 
+	"github.com/containernetworking/cni/pkg/types/current"
+	"github.com/containernetworking/plugins/pkg/ip"
+	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/rs/zerolog/log"
 	"github.com/threefoldtech/testv2/modules/network/bridge"
 	"github.com/threefoldtech/testv2/modules/network/wireguard"
+	"github.com/vishvananda/netlink"
 
 	"github.com/threefoldtech/testv2/modules/network/namespace"
 
 	"github.com/threefoldtech/testv2/modules"
+	testip "github.com/threefoldtech/testv2/modules/network/ip"
 )
 
 type networker struct {
+	nodeID      modules.NodeID
 	storageDir  string
 	netResAlloc NetResourceAllocator
 }
 
+// NewNetworker create a new modules.Networker that can be used with zbus
 func NewNetworker(storageDir string, allocator NetResourceAllocator) modules.Networker {
 	return &networker{
 		storageDir:  storageDir,
@@ -30,81 +37,168 @@ func NewNetworker(storageDir string, allocator NetResourceAllocator) modules.Net
 
 var _ modules.Networker = (*networker)(nil)
 
-func (n *networker) GetNetResource(id string) (modules.NetResource, error) {
+// GetNetwork implements modules.Networker interface
+func (n *networker) GetNetwork(id string) (*modules.Network, error) {
 	// TODO check signature
 	return n.netResAlloc.Get(id)
 }
 
-func (n *networker) ApplyNetResource(netID modules.NetID, resource modules.NetResource) error {
-	return applyNetResource(n.storageDir, netID, resource)
+// ApplyNetResource implements modules.Networker interface
+func (n *networker) ApplyNetResource(network *modules.Network) error {
+
+	var resource *modules.NetResource
+	for _, res := range network.Resources {
+		if res.NodeID == n.nodeID {
+			resource = &res
+			break
+		}
+	}
+	if resource == nil {
+		return fmt.Errorf("not network resource for this node: %s", n.nodeID.ID)
+	}
+
+	return applyNetResource(n.storageDir, network.NetID, resource, network.AllocationNR)
 }
 
-func applyNetResource(storageDir string, netID modules.NetID, netRes modules.NetResource) error {
-	network := string(netID)
-	if err := createNetwork(network); err != nil {
+func (n *networker) DeleteNetResource(network *modules.Network) error {
+	var resource *modules.NetResource
+	for _, res := range network.Resources {
+		if res.NodeID == n.nodeID {
+			resource = &res
+			break
+		}
+	}
+	if resource == nil {
+		return fmt.Errorf("not network resource for this node: %s", n.nodeID.ID)
+	}
+	return deleteNetResource(resource, network.AllocationNR)
+}
+
+func applyNetResource(storageDir string, netID modules.NetID, netRes *modules.NetResource, allocNr int8) error {
+	if err := createNetworkResource(netID, netRes, allocNr); err != nil {
 		return err
 	}
 
-	storage := filepath.Join(storageDir, network)
-	if err := configureWG(storage, network, netRes); err != nil {
+	if _, err := configureWG(storageDir, netRes, allocNr); err != nil {
 		return err
 	}
 	return nil
 }
 
-// createNetwork creates a network namespace and a bridge
+// createNetworkResource creates a network namespace and a bridge
 // and a wireguard interface and then move it interface inside
 // the net namespace
-func createNetwork(name string) error {
+func createNetworkResource(netID modules.NetID, resource *modules.NetResource, allorNr int8) error {
 	var (
-		netnsName  = netnsName(name)
-		bridgeName = bridgeName(name)
-		wgName     = wgName(name)
+		nibble     = testip.NewNibble(resource.Prefix, allorNr)
+		netnsName  = nibble.NetworkName()
+		bridgeName = nibble.BridgeName()
+		wgName     = nibble.WiregardName()
+		vethName   = nibble.VethName()
 	)
 
-	if !namespace.Exists(netnsName) {
-		log.Info().Str("name", netnsName).Msg("create network namespace")
-		_, err := namespace.Create(netnsName)
-		if err != nil {
-			return err
-		}
-
-		log.Info().Str("name", wgName).Msg("create wireguard interface")
-		wg, err := wireguard.New(wgName)
-		if err != nil {
-			return err
-		}
-
-		log.Info().
-			Str("wg", wgName).
-			Str("namespace", netnsName).
-			Msg("move wireguard into network namespace")
-		if err := namespace.SetLink(wg, netnsName); err != nil {
-			return err
-		}
+	log.Info().Str("bridge", bridgeName).Msg("Create bridge")
+	br, err := bridge.New(bridgeName)
+	if err != nil {
+		return err
 	}
 
-	if !bridge.Exists(bridgeName) {
-		if _, err := bridge.New(bridgeName); err != nil {
+	log.Info().Str("namesapce", netnsName).Msg("Create namesapce")
+	netns, err := namespace.Create(netnsName)
+	if err != nil {
+		return err
+	}
+
+	hostIface := &current.Interface{}
+	var handler = func(hostNS ns.NetNS) error {
+		log.Info().
+			Str("namespace", netnsName).
+			Str("veth", vethName).
+			Msg("Create veth pair in net namespace")
+		hostVeth, containerVeth, err := ip.SetupVeth(vethName, 1500, hostNS)
+		if err != nil {
 			return err
 		}
+		hostIface.Name = hostVeth.Name
+
+		link, err := netlink.LinkByName(containerVeth.Name)
+		if err != nil {
+			return err
+		}
+
+		ipnetv6 := &resource.Prefix
+		a, b, err := nibble.ToV4()
+		if err != nil {
+			return err
+		}
+		ip, ipnetv4, err := net.ParseCIDR(fmt.Sprintf("10.%d.%d.1/24", a, b))
+		if err != nil {
+			return err
+		}
+		ipnetv4.IP = ip
+
+		for _, ipnet := range []*net.IPNet{ipnetv6, ipnetv4} {
+			log.Info().Str("addr", ipnet.String()).Msg("set address on veth interface")
+			addr := &netlink.Addr{IPNet: ipnet, Label: ""}
+			if err = netlink.AddrAdd(link, addr); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+	if err := netns.Do(handler); err != nil {
+		return err
+	}
+
+	hostVeth, err := netlink.LinkByName(hostIface.Name)
+	if err != nil {
+		return err
+	}
+
+	log.Info().
+		Str("veth", vethName).
+		Str("bridge", bridgeName).
+		Msg("attach veth to bridge")
+	if err := bridge.AttachNic(hostVeth, br); err != nil {
+		return err
+	}
+
+	log.Info().Str("wg", wgName).Msg("create wireguard interface")
+	wg, err := wireguard.New(wgName)
+	if err != nil {
+		return err
+	}
+
+	log.Info().
+		Str("wg", wgName).
+		Str("namespace", netnsName).
+		Msg("move wireguard into network namespace")
+	if err := namespace.SetLink(wg, netnsName); err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func deleteNetwork(network string) error {
-	if err := bridge.Delete(bridgeName(network)); err != nil {
+func deleteNetResource(resource *modules.NetResource, allocNr int8) error {
+	var (
+		nibble     = testip.NewNibble(resource.Prefix, allocNr)
+		netnsName  = nibble.NetworkName()
+		bridgeName = nibble.BridgeName()
+	)
+	if err := bridge.Delete(bridgeName); err != nil {
 		return err
 	}
-	return namespace.Delete(netnsName(network))
+	return namespace.Delete(netnsName)
 }
 
-func configureWG(storageDir, network string, netRes modules.NetResource) error {
+func configureWG(storageDir string, resource *modules.NetResource, allocNr int8) (wgtypes.Key, error) {
 	var (
-		netnsName   = netnsName(network)
-		wgName      = wgName(network)
-		storagePath = filepath.Join(storageDir, network)
+		nibble      = testip.NewNibble(resource.Prefix, allocNr)
+		netnsName   = nibble.NetworkName()
+		wgName      = nibble.WiregardName()
+		storagePath = filepath.Join(storageDir, nibble.Hex())
 		key         wgtypes.Key
 		err         error
 	)
@@ -113,73 +207,64 @@ func configureWG(storageDir, network string, netRes modules.NetResource) error {
 	if err != nil {
 		key, err = wireguard.GenerateKey(storagePath)
 		if err != nil {
-			return err
+			return key, err
 		}
-	}
-
-	// enter container net ns
-	nsCtx := namespace.NSContext{}
-	if err := nsCtx.Enter(netnsName); err != nil {
-		return err
-	}
-
-	wg, err := wireguard.GetByName(wgName)
-	if err != nil {
-		return err
 	}
 
 	// configure wg iface
-	peers := make([]wireguard.Peer, len(netRes.Connected))
-	for i, connected := range netRes.Connected {
-		if connected.Type != modules.ConnTypeWireguard {
+	peers := make([]wireguard.Peer, len(resource.Connected))
+	for i, peer := range resource.Connected {
+		if peer.Type != modules.ConnTypeWireguard {
 			continue
 		}
 
-		conn := connected.Connection
-
-		var endpoint string
-		if conn.Peer.To16() != nil {
-			endpoint = fmt.Sprintf("[%s]:%d", conn.Peer.String(), conn.PeerPort)
-		} else {
-			endpoint = fmt.Sprintf("%s:%d", conn.Peer.String(), conn.PeerPort)
+		a, b, err := nibble.ToV4()
+		if err != nil {
+			return key, err
 		}
-
 		peers[i] = wireguard.Peer{
-			PublicKey:  conn.Key,
-			Endpoint:   endpoint,
-			AllowedIPs: []string{"0.0.0.0/0"},
+			PublicKey: peer.Connection.Key,
+			Endpoint:  endpoint(peer),
+			AllowedIPs: []string{
+				fmt.Sprintf("fe80::%s/128", nibble.Hex()),
+				fmt.Sprintf("172.16.%d.%d/32", a, b),
+			},
 		}
 	}
 
-	wgIPNet, err := wgIP(netRes.Prefix)
+	netns, err := namespace.GetByName(netnsName)
 	if err != nil {
-		return err
+		return key, err
 	}
 
-	log.Info().Msg("configure wireguard interface")
+	var handler = func(_ ns.NetNS) error {
 
-	err = wg.Configure(wgIPNet.String(), key.String(), peers)
-	if err != nil {
-		nsCtx.Exit()
-		return err
+		wg, err := wireguard.GetByName(wgName)
+		if err != nil {
+			return err
+		}
+
+		log.Info().Msg("configure wireguard interface")
+		if err = wg.Configure(resource.LinkLocal.String(), key.String(), peers); err != nil {
+			return err
+		}
+		return nil
+	}
+	if err := netns.Do(handler); err != nil {
+		return key, err
 	}
 
-	// exit containe net ns
-	if err := nsCtx.Exit(); err != nil {
-		return err
+	return key, nil
+}
+
+func endpoint(peer modules.Connected) string {
+	var endpoint string
+	if peer.Connection.IP.To16() != nil {
+		endpoint = fmt.Sprintf("[%s]:%d", peer.Connection.IP.String(), peer.Connection.Port)
+	} else {
+		endpoint = fmt.Sprintf("%s:%d", peer.Connection.IP.String(), peer.Connection.Port)
 	}
-
-	return nil
-}
-
-func bridgeName(name string) string {
-	return fmt.Sprintf("br%s", name)
-}
-func wgName(name string) string {
-	return fmt.Sprintf("wg%s", name)
-}
-func netnsName(name string) string {
-	return fmt.Sprintf("ns%s", name)
+	return endpoint
 }
 
 func wgIP(prefix net.IPNet) (*net.IPNet, error) {
@@ -190,80 +275,4 @@ func wgIP(prefix net.IPNet) (*net.IPNet, error) {
 		return nil, err
 	}
 	return ipnet, nil
-}
-
-type NetResourceAllocator interface {
-	Get(txID string) (modules.NetResource, error)
-}
-
-// type httpNetResourceAllocator struct {
-// 	baseURL string
-// }
-
-// func NewHTTPNetResourceAllocator(url string) *httpNetResourceAllocator {
-// 	return &httpNetResourceAllocator{url}
-// }
-
-// func (a *httpNetResourceAllocator) Get(txID string) (modules.NetResource, error) {
-// 	netRes := modules.NetResource{}
-
-// 	url, err := joinURL(a.baseURL, txID)
-
-// 	resp, err := http.Get(url)
-// 	if err != nil {
-// 		return netRes, err
-// 	}
-// 	defer resp.Body.Close()
-
-// 	if err := json.NewDecoder(resp.Body).Decode(&netRes); err != nil {
-// 		return netRes, err
-// 	}
-
-// 	return netRes, nil
-// }
-
-// func joinURL(base, path string) (string, error) {
-// 	u, err := url.Parse(base)
-// 	if err != nil {
-// 		return "nil", err
-// 	}
-// 	u.Path = filepath.Join(u.Path, path)
-// 	return u.String(), nil
-// }
-
-type TestNetResourceAllocator struct {
-	Resource modules.NetResource
-}
-
-func NewTestNetResourceAllocator() NetResourceAllocator {
-	return &TestNetResourceAllocator{
-		Resource: modules.NetResource{
-			NodeID: modules.NodeID("supernode"),
-			Prefix: MustParseCIDR("2a02:1802:5e:f002::/64"),
-			Connected: []modules.Connected{
-				{
-					Type:   modules.ConnTypeWireguard,
-					Prefix: MustParseCIDR("2a02:1802:5e:cc02::/64"),
-					Connection: modules.Wireguard{
-						NICName: "cc02",
-						Peer:    net.ParseIP("2001::1"),
-						Key:     "",
-						// LinkLocal: net.
-					},
-				},
-			},
-		},
-	}
-}
-
-func (a *TestNetResourceAllocator) Get(txID string) (modules.NetResource, error) {
-	return a.Resource, nil
-}
-
-func MustParseCIDR(cidr string) net.IPNet {
-	_, ipnet, err := net.ParseCIDR(cidr)
-	if err != nil {
-		panic(err)
-	}
-	return *ipnet
 }
