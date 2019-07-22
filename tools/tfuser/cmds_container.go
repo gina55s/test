43ca7554bfc42f179eb187d058e40482580bf457
Christophe de Carvalho <zaibon@users.noreply.github.com>
2019-07-22 10:22:23 +0200

    Squashed 8 commits from 2019-07-22
    - create package crypto
    - extract TNO manipulation logic out of the tnodb_mock and move it into
    - update provisiond to use a full TNO as network provisioning schema
    - refactor tfuser to generate TNO locally before
    - go mod tidy
    - tfuser: refactor tfuser to decouple generattion of provision schema and
    - go mod tidy
    - Create notes_20192207.md

---
+++ tools/tfuser/cmds_container.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/threefoldtech/testv2/modules/provision"
+	"github.com/urfave/cli"
+)
+
+func generateContainer(c *cli.Context) error {
+
+	envs, err := splitEnvs(c.StringSlice("envs"))
+	if err != nil {
+		return err
+	}
+
+	mounts, err := splitMounts(c.StringSlice("mounts"))
+	if err != nil {
+		return err
+	}
+
+	container := provision.Container{
+		FList:       c.String("flist"),
+		Env:         envs,
+		Entrypoint:  c.String("entrypoint"),
+		Interactive: c.Bool("corex"),
+		Mounts:      mounts,
+		Network: provision.Network{
+			NetwokID: c.String("network"),
+		},
+	}
+
+	if err := validateContainer(container); err != nil {
+		return err
+	}
+
+	p, err := embed(container, provision.ContainerReservation)
+	if err != nil {
+		return err
+	}
+
+	return output(c.GlobalString("output"), p)
+}
+
+func validateContainer(c provision.Container) error {
+	if c.FList == "" {
+		return fmt.Errorf("flist cannot be empty")
+	}
+	return nil
+}
+
+func splitEnvs(envs []string) (map[string]string, error) {
+	out := make(map[string]string, len(envs))
+
+	for _, env := range envs {
+		ss := strings.SplitN(env, "=", 2)
+		if len(ss) != 2 {
+			return nil, fmt.Errorf("envs flag mal formatted: %v", env)
+		}
+		out[ss[0]] = ss[1]
+	}
+
+	return out, nil
+}
+
+func splitMounts(mounts []string) ([]provision.Mount, error) {
+	out := make([]provision.Mount, 0, len(mounts))
+
+	for _, mount := range mounts {
+		ss := strings.SplitN(mount, ":", 2)
+		if len(ss) != 2 {
+			return nil, fmt.Errorf("mounts flag mal formatted: %v", mount)
+		}
+
+		out = append(out, provision.Mount{
+			VolumeID:   ss[0],
+			Mountpoint: ss[1],
+		})
+	}
+
+	return out, nil
+}
