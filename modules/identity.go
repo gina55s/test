1f3728688404bd6d5adec3b14569e0e2233ffc32
Muhamad Azmy <muhamad.azmy@codescalers.com>
2019-07-30 14:15:11 +0200

    Squashed 17 commits from 2019-07-30
    - crypto: add Sign and Verify methods (#154)
    - provision: change bcdb provision logic
    - storage: add a Path method on the storage interface
    - provision: check the ownership of a volume when mounting it inside a
    - provisiond: try to provision existing workload at boot
    - go mod tidy all binaries
    - update documentation about basic network creation
    - networkd.yaml: re-add missing exec
    - tfuser: print the user identity after generation
    - tfuser: use text/template instead of html/template
    - Update cmds/identityd/main.go
    - Update modules/identity.go
    - Update modules/identity.go
    - Update modules/identity/identityd.go
    - Update modules/identity/identityd.go
    - Use crypto module
    - Apply review comments

--- modules/identity.go
+++ modules/identity.go
@@ -4,7 +4,7 @@ package modules
 //go:generate zbusc -module identity -version 0.0.1 -name manager -package stubs github.com/threefoldtech/testv2/modules+IdentityManager stubs/identity_stub.go
 
 // Identifier is the interface that defines
-// how an object can be used an identity
+// how an object can be used as an identity
 type Identifier interface {
 	Identity() string
 }
@@ -27,11 +27,11 @@ type IdentityManager interface {
 	// that the node is booted with. An error is returned if the farmer id is not configured
 	FarmID() (StrIdentifier, error)
 
-	// Sign
-	Sign(data []byte) ([]byte, error)
+	// Sign signs the message with privateKey and returns a signature.
+	Sign(message []byte) ([]byte, error)
 
-	// Verify
-	Verify(data, sig []byte) error
+	// Verify reports whether sig is a valid signature of message by publicKey.
+	Verify(message, sig []byte) error
 
 	// Encrypt, Decrypt ?
 }
