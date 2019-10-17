992d6416ab83adf9e1026741b03e01ca0e0d9c08
Muhamad Azmy <muhamad.azmy+git@gmail.com>
2019-10-17 15:36:19 +0200

    Squashed 4 commits from 2019-10-17
    - zlf: log and exit instead of panic
    - Merge pull request #332 from threefoldtech/master-upgrade-uninstall
    - contd: read environment variable from .stratup.toml if it exists and ensure /etc/resolv.conf (#318)
    - Update docs for identityd

--- cmds/identityd/main.go
+++ cmds/identityd/main.go
@@ -2,69 +2,241 @@ package main
 
 import (
 	"context"
-	"flag"
 	"os"
-	"path"
+	"os/signal"
+	"path/filepath"
+	"time"
 
 	"github.com/pkg/errors"
-	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/test/pkg/gedis"
 
 	"github.com/cenkalti/backoff/v3"
+	"github.com/threefoldtech/test/pkg"
+	"github.com/threefoldtech/test/pkg/environment"
+	"github.com/threefoldtech/test/pkg/identity"
+
+	"github.com/threefoldtech/test/pkg/zinit"
+
+	"flag"
+
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"github.com/threefoldtech/test/pkg/environment"
-	"github.com/threefoldtech/test/pkg/gedis"
-	"github.com/threefoldtech/test/pkg/identity"
+	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/test/pkg/stubs"
+	"github.com/threefoldtech/test/pkg/upgrade"
 	"github.com/threefoldtech/test/pkg/utils"
 	"github.com/threefoldtech/test/pkg/version"
 )
 
 const (
-	seedName = "seed.txt"
-	module   = "identityd"
-	workers  = 10
+	redisSocket = "unix:///var/run/redis.sock"
+	zinitSocket = "/var/run/zinit.sock"
 )
+
+const (
+	module   = "identityd"
+	seedName = "seed.txt"
+)
+
+// setup is a sanity check function, the whole purpose of this
+// is to make sure at least required services are running in case
+// of upgrade failure
+// for example, in case of upgraded crash after it already stopped all
+// the services for upgrade.
+func setup(zinit *zinit.Client) error {
+	for _, required := range []string{"redis", "flistd"} {
+		if err := zinit.StartWait(5*time.Second, required); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// SafeUpgrade makes sure upgrade daemon is not interrupted
+// While
+func SafeUpgrade(upgrader *upgrade.Upgrader) error {
+	ch := make(chan os.Signal)
+	defer close(ch)
+	defer signal.Stop(ch)
+
+	// try to upgraded to latest
+	// but mean while also make sure the daemon can not be killed by a signal
+	signal.Notify(ch)
+	return upgrader.Upgrade()
+}
+
+// This daemon startup has the follow flow:
+// 1. Do upgrade to latest version (this might means it needs to restart itself)
+// 2. Register the node to BCDB
+// 3. start zbus server to serve identity interface
+// 4. Start watcher for new version
+// 5. On update, re-register the node with new version to BCDB
 
 func main() {
 	var (
-		root         string
-		msgBrokerCon string
-		ver          bool
+		broker   string
+		root     string
+		interval int
+		ver      bool
+		debug    bool
 	)
 
 	flag.StringVar(&root, "root", "/var/cache/modules/identityd", "root working directory of the module")
-	flag.StringVar(&msgBrokerCon, "broker", "unix:///var/run/redis.sock", "connection string to the message broker")
+	flag.StringVar(&broker, "broker", redisSocket, "connection string to broker")
+	flag.IntVar(&interval, "interval", 600, "interval in seconds between update checks, default to 600")
 	flag.BoolVar(&ver, "v", false, "show version and exit")
+	flag.BoolVar(&debug, "d", false, "when set, no self update is done before upgradeing")
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	flag.Parse()
 	if ver {
 		version.ShowAndExit(false)
 	}
 
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-
-	store, err := bcdbClient()
+	zinit, err := zinit.New(zinitSocket)
 	if err != nil {
-		log.Fatal().Err(err).Send()
+		log.Fatal().Err(err).Msg("failed to connect to zinit")
 	}
 
-	if err := os.MkdirAll(root, 0755); err != nil {
-		log.Fatal().Err(err).Msg("failed to create module root")
-	}
-
-	server, err := zbus.NewRedisServer(module, msgBrokerCon, workers)
+	cl, err := zbus.NewRedisClient(broker)
 	if err != nil {
-		log.Fatal().Err(err).Msg("failed to create zbus server")
+		log.Error().Err(err).Msg("fail to connect to broker")
+		return
 	}
 
-	manager, err := identity.NewManager(path.Join(root, seedName))
+	flister := stubs.NewFlisterStub(cl)
+
+	upgrader := upgrade.Upgrader{
+		FLister:      flister,
+		Zinit:        zinit,
+		NoSelfUpdate: debug,
+	}
+
+	bootMethod := upgrade.DetectBootMethod()
+
+	// 2. Register the node to BCDB
+	// at this point we are running latest version
+	idMgr, err := identityMgr(root)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to create identity manager")
 	}
 
-	farmID, err := manager.FarmID()
+	idStore, err := bcdbClient()
 	if err != nil {
-		log.Fatal().Err(err).Msg("fail to read farmer id from kernel parameters")
+		log.Fatal().Err(err).Msg("failed to create identity client")
+	}
+
+	var current string
+	if bootMethod == upgrade.BootMethodFList {
+		v, err := upgrader.Version()
+		if err != nil {
+			//NOTE: this is set to error intentionally (not fatal)
+			//this will cause version to be 0.0.0 and will force an
+			//immediate update of the flist to latest
+			log.Error().Err(err).Msg("failed to read current version")
+		}
+		current = v.String()
+	} else {
+		current = "not booted from flist"
+	}
+
+	nodeID := idMgr.NodeID()
+	farmID, err := idMgr.FarmID()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to read farm ID")
+	}
+
+	register := func(v string) error {
+		return registerNode(nodeID, farmID, v, idStore)
+	}
+
+	if err := backoff.Retry(func() error {
+		return register(current)
+	}, backoff.NewExponentialBackOff()); err != nil {
+		log.Error().Err(err).Msg("failed to register node")
+	} else {
+		log.Info().Str("version", current).Msg("node registered successfully")
+	}
+
+	// 3. start zbus server to serve identity interface
+	server, err := zbus.NewRedisServer(module, broker, 1)
+	if err != nil {
+		log.Fatal().Msgf("fail to connect to message broker server: %v\n", err)
+	}
+
+	server.Register(zbus.ObjectID{Name: "manager", Version: "0.0.1"}, idMgr)
+
+	ctx, cancel := utils.WithSignal(context.Background())
+	// register the cancel function with defer if the process stops because of a update
+	defer cancel()
+
+	go func() {
+		if err := server.Run(ctx); err != nil && err != context.Canceled {
+			log.Error().Err(err).Msg("unexpected error")
+		}
+	}()
+
+	utils.OnDone(ctx, func(_ error) {
+		log.Info().Msg("shutting down")
+	})
+
+	if bootMethod != upgrade.BootMethodFList {
+		log.Info().Msg("node is not booted from an flist. upgrade is not supported")
+		<-ctx.Done()
+		return
+	}
+
+	// 4. Start watcher for new version
+	log.Info().Msg("start upgrade daemon")
+	ticker := time.NewTicker(time.Second * time.Duration(interval))
+	defer ticker.Stop()
+
+	for {
+		err := SafeUpgrade(&upgrader)
+		if err == upgrade.ErrRestartNeeded {
+			log.Info().Msg("restarting upgraded")
+			return
+		} else if err != nil {
+			//TODO: crash or continue!
+			log.Error().Err(err).Msg("upgrade failed")
+		}
+
+		version, err := upgrader.Version()
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to read current version")
+		}
+
+		if version.String() != current {
+			log.Info().Str("version", version.String()).Msg("new version")
+
+			if err := backoff.Retry(func() error {
+				return register(version.String())
+			}, backoff.NewExponentialBackOff()); err != nil {
+				log.Error().Err(err).Msg("failed to register node")
+			} else {
+				log.Info().Str("version", version.String()).Msg("node registered successfully")
+			}
+
+			current = version.String()
+		}
+
+		select {
+		case <-ticker.C:
+			log.Debug().Msg("checking for updates")
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func identityMgr(root string) (pkg.IdentityManager, error) {
+	seedPath := filepath.Join(root, seedName)
+
+	manager, err := identity.NewManager(seedPath)
+	if err != nil {
+		return nil, err
 	}
 
 	env := environment.Get()
@@ -79,43 +251,7 @@ func main() {
 		Str("farmer_id", env.FarmerID).
 		Msg("farmer identified")
 
-	// Node registration can happen in the background.
-	go func() {
-
-		f := func() error {
-			log.Info().Msg("start registration of the node")
-			_, err := store.RegisterNode(nodeID, farmID, "fixme")
-			if err != nil {
-				log.Error().Err(err).Msg("fail to register node identity")
-				return err
-			}
-			return nil
-		}
-
-		if err := backoff.Retry(f, backoff.NewExponentialBackOff()); err == nil {
-			log.Info().Msg("node registered successfully")
-		}
-	}()
-
-	if err := server.Register(zbus.ObjectID{
-		Name:    "manager",
-		Version: "0.0.1",
-	}, manager); err != nil {
-		log.Fatal().Err(err).Msg("failed to register identity manager")
-	}
-
-	log.Info().
-		Str("broker", msgBrokerCon).
-		Msg("starting identity module")
-
-	ctx, _ := utils.WithSignal(context.Background())
-	utils.OnDone(ctx, func(_ error) {
-		log.Info().Msg("shutting down")
-	})
-
-	if err := server.Run(ctx); err != nil && err != context.Canceled {
-		log.Fatal().Err(err).Msg("unexpected error")
-	}
+	return manager, nil
 }
 
 // instantiate the proper client based on the running mode
@@ -133,4 +269,14 @@ func bcdbClient() (identity.IDStore, error) {
 		return nil, errors.Wrap(err, "fail to connect to BCDB")
 	}
 	return store, nil
+}
+
+func registerNode(nodeID, farmID pkg.Identifier, version string, store identity.IDStore) error {
+	log.Info().Str("version", version).Msg("start registration of the node")
+
+	_, err := store.RegisterNode(nodeID, farmID, version)
+	if err != nil {
+		return err
+	}
+	return nil
 }
