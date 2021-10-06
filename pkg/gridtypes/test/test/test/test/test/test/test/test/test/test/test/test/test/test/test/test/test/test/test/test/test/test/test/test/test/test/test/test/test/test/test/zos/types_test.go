cdca16beff625239cf3b2da6bb5a83d199fc6c32
xmonader <xmonader@gmail.com>
2021-10-06 19:52:20 +0200

    Merge: Merge pull request #1414 from threefoldtech/cloud-container-flist
    make a new mount if flist is updated

---
+++ pkg/gridtypes/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/zos/types_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/threefoldtech/test/pkg/gridtypes"
+)
+
+func TestWorkloadData(t *testing.T) {
+	require := require.New(t)
+
+	wl := gridtypes.Workload{
+		Type: ZMountType,
+		Data: json.RawMessage(`{"size": 10}`),
+	}
+
+	data, err := wl.WorkloadData()
+	require.NoError(err)
+
+	require.IsType(&ZMount{}, data)
+	volume := data.(*ZMount)
+
+	require.Equal(gridtypes.Unit(10), volume.Size)
+}
+
+func TestWorkloadValidation(t *testing.T) {
+	require := require.New(t)
+
+	wl := gridtypes.Workload{
+		Type: ZMountType,
+		Name: "name",
+		Data: json.RawMessage(`{"size": 10}`),
+	}
+
+	err := wl.Valid(nil)
+	require.NoError(err)
+
+}
