2c28208ced5c8914e836657626e560dc3df9652f
Muhamad Azamy <muhamad@incubaid.com>
2021-10-25 17:55:23 +0200

    Squashed 4 commits from 2021-10-25
    - Listen to public config events and apply changes (#1441)
    - remove deletion of routing rules
    - retry afger failure to set public config
    - always report consumption on provisiond start

---
+++ cmds/modules/noded/main.go
@@ -0,0 +1,149 @@
+package noded
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
+
+	"github.com/threefoldtech/test/pkg/capacity"
+	"github.com/threefoldtech/test/pkg/environment"
+	"github.com/threefoldtech/test/pkg/monitord"
+	"github.com/threefoldtech/test/pkg/rmb"
+	"github.com/threefoldtech/test/pkg/stubs"
+	"github.com/threefoldtech/test/pkg/utils"
+
+	"github.com/rs/zerolog/log"
+
+	"github.com/threefoldtech/zbus"
+)
+
+const module = "monitor"
+
+// Module is entry point for module
+var Module cli.Command = cli.Command{
+	Name:  "noded",
+	Usage: "reports the node total resources",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "broker",
+			Usage: "connection string to the message `BROKER`",
+			Value: "unix:///var/run/redis.sock",
+		},
+	},
+	Action: action,
+}
+
+func mon(ctx context.Context, server zbus.Server) {
+	system, err := monitord.NewSystemMonitor(2 * time.Second)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to initialize system monitor")
+	}
+	host, err := monitord.NewHostMonitor(2 * time.Second)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to initialize host monitor")
+	}
+
+	server.Register(zbus.ObjectID{Name: "host", Version: "0.0.1"}, host)
+	server.Register(zbus.ObjectID{Name: "system", Version: "0.0.1"}, system)
+}
+
+func action(cli *cli.Context) error {
+	var (
+		msgBrokerCon string = cli.String("broker")
+	)
+
+	env := environment.MustGet()
+
+	redis, err := zbus.NewRedisClient(msgBrokerCon)
+	if err != nil {
+		return errors.Wrap(err, "fail to connect to message broker server")
+	}
+
+	server, err := zbus.NewRedisServer(module, msgBrokerCon, 1)
+	if err != nil {
+		return errors.Wrap(err, "fail to connect to message broker server")
+	}
+
+	ctx, _ := utils.WithSignal(context.Background())
+	utils.OnDone(ctx, func(_ error) {
+		log.Info().Msg("shutting down")
+	})
+
+	mon(ctx, server)
+
+	go func() {
+		if err := server.Run(ctx); err != nil && err != context.Canceled {
+			log.Error().Err(err).Msg("unexpected error")
+		}
+	}()
+
+	oracle := capacity.NewResourceOracle(stubs.NewStorageModuleStub(redis))
+	cap, err := oracle.Total()
+	if err != nil {
+		return errors.Wrap(err, "failed to get node capacity")
+	}
+
+	bus, err := rmb.New(msgBrokerCon)
+	if err != nil {
+		return errors.Wrap(err, "failed to initialize message bus server")
+	}
+
+	dmi, err := oracle.DMI()
+	if err != nil {
+		return errors.Wrap(err, "failed to get dmi information")
+	}
+
+	hypervisor, err := oracle.GetHypervisor()
+	if err != nil {
+		return errors.Wrap(err, "failed to get hypervisors")
+	}
+
+	bus.WithHandler("test.system.dmi", func(ctx context.Context, payload []byte) (interface{}, error) {
+		return dmi, nil
+	})
+
+	bus.WithHandler("test.system.hypervisor", func(ctx context.Context, payload []byte) (interface{}, error) {
+		return hypervisor, nil
+	})
+
+	// answer calls for dmi
+	go func() {
+		if err := bus.Run(ctx); err != nil {
+			log.Fatal().Err(err).Msg("message bus handler failure")
+		}
+	}()
+
+	node, twin, err := registration(ctx, redis, cap)
+	if err != nil {
+		return errors.Wrap(err, "failed during node registration")
+	}
+
+	// TODO: monitor change to yggdrasil Ip and update the twin according
+	log.Info().Uint32("node", node).Uint32("twin", twin).Msg("node registered")
+
+	go func() {
+		for {
+			if err := public(ctx, node, redis); err != nil {
+				log.Error().Err(err).Msg("setting public config failed")
+				<-time.After(10 * time.Second)
+			}
+		}
+	}()
+
+	// uptime update
+	go func() {
+		for {
+			if err := uptime(ctx, redis); err != nil {
+				log.Error().Err(err).Msg("sending uptime failed")
+				<-time.After(10 * time.Second)
+			}
+		}
+	}()
+
+	log.Info().Uint32("twin", twin).Msg("node has been registered")
+	log.Debug().Msg("start message bus")
+
+	return runMsgBus(ctx, twin, env.SubstrateURL)
+}
---
+++ cmds/modules/noded/twin.go
@@ -0,0 +1,26 @@
+package noded
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/go-rmb"
+)
+
+func runMsgBus(ctx context.Context, twin uint32, substrate string) error {
+	// todo: make it argument or parse from broker
+	const redis = "/var/run/redis.sock"
+	app, err := rmb.NewServer(substrate, redis, int(twin))
+	if err != nil {
+		return err
+	}
+
+	log.Info().Uint32("twin", twin).Msg("starting twin")
+
+	if err := app.Serve(ctx); err != nil && !errors.Is(err, context.Canceled) {
+		return err
+	}
+
+	return nil
+}
