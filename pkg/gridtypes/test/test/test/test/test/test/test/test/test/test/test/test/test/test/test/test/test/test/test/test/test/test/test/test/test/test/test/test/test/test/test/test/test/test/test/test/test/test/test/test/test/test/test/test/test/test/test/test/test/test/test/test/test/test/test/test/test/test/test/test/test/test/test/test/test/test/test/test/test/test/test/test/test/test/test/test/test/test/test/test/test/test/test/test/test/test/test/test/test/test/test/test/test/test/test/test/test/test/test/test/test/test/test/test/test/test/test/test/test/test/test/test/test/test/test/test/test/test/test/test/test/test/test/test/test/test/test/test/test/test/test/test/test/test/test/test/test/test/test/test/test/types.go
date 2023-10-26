de5ab3934095475038427b03d8d97205caeb8c1b
Muhamad Azamy <muhamad@incubaid.com>
2023-10-26 10:50:54 +0200

    Squashed 4 commits from 2023-10-26
    - unify rfs and zufs pkgs (#2091)
    - Rework release (#2082)
    - the last .0 is importnat for version parsing
    - make sure nodes don't get into dead update lock

---
+++ pkg/gridtypes/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/test/types.go
@@ -0,0 +1,66 @@
+package test
+
+import (
+	"fmt"
+
+	"github.com/threefoldtech/test/pkg/gridtypes"
+)
+
+const (
+	// ZMountType type
+	ZMountType gridtypes.WorkloadType = "zmount"
+	// NetworkType type
+	NetworkType gridtypes.WorkloadType = "network"
+	// ZDBType type
+	ZDBType gridtypes.WorkloadType = "zdb"
+	// ZMachineType type
+	ZMachineType gridtypes.WorkloadType = "zmachine"
+	//PublicIPv4Type type [deprecated]
+	PublicIPv4Type gridtypes.WorkloadType = "ipv4"
+	//PublicIPType type is the new way to assign public ips
+	// to a VM. this has flags (V4, and V6) that has to be set.
+	PublicIPType gridtypes.WorkloadType = "ip"
+	// GatewayNameProxyType type
+	GatewayNameProxyType gridtypes.WorkloadType = "gateway-name-proxy"
+	// GatewayFQDNProxyType type
+	GatewayFQDNProxyType gridtypes.WorkloadType = "gateway-fqdn-proxy"
+	// QuantumSafeFSType type
+	QuantumSafeFSType gridtypes.WorkloadType = "qsfs"
+)
+
+func init() {
+	// network is a sharable type, which means for a single
+	// twin, the network objects can be 'used' from different
+	// deployments.
+	gridtypes.RegisterSharableType(NetworkType, Network{})
+	gridtypes.RegisterType(ZMountType, ZMount{})
+	gridtypes.RegisterType(ZDBType, ZDB{})
+	gridtypes.RegisterType(ZMachineType, ZMachine{})
+	gridtypes.RegisterType(PublicIPv4Type, PublicIP4{})
+	gridtypes.RegisterType(PublicIPType, PublicIP{})
+	gridtypes.RegisterType(GatewayNameProxyType, GatewayNameProxy{})
+	gridtypes.RegisterType(GatewayFQDNProxyType, GatewayFQDNProxy{})
+	gridtypes.RegisterType(QuantumSafeFSType, QuantumSafeFS{})
+}
+
+// DeviceType is the actual type of hardware that the storage device runs on,
+// i.e. SSD or HDD
+type DeviceType string
+
+// Known device types
+const (
+	SSDDevice DeviceType = "ssd"
+	HDDDevice DeviceType = "hdd"
+)
+
+func (d DeviceType) String() string {
+	return string(d)
+}
+
+// Valid validates device type
+func (d DeviceType) Valid() error {
+	if d != SSDDevice && d != HDDDevice {
+		return fmt.Errorf("invalid device type")
+	}
+	return nil
+}
