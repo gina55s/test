73d05cd9cddd59caaf590afcc29112731875c5c9
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2025-01-15 18:15:06 +0200

    Squashed 3 commits from 2025-01-15
    - Merge pull request #2514 from threefoldtech/document-node-client
    - Merge pull request #2512 from threefoldtech/document_version_and_versiond
    - Merge pull request #2481 from threefoldtech/main_addLocationEndpoint

---
+++ pkg/test_api/zos_api/location.go
@@ -0,0 +1,27 @@
+package testapi
+
+import (
+	"context"
+
+	"github.com/patrickmn/go-cache"
+	"github.com/threefoldtech/test/pkg/geoip"
+)
+
+const (
+	locationCacheKey = "location"
+)
+
+func (g *ZosAPI) locationGet(ctx context.Context, payload []byte) (interface{}, error) {
+	if loc, found := g.inMemCache.Get(locationCacheKey); found {
+		return loc, nil
+	}
+
+	loc, err := geoip.Fetch()
+	if err != nil {
+		return nil, err
+	}
+
+	g.inMemCache.Set(locationCacheKey, loc, cache.DefaultExpiration)
+
+	return loc, nil
+}
